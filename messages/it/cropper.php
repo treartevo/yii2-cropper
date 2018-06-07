ad29ca897d60d335b339baa003c39494a607a829
treartevo <software@treart-evolution.com>
2018-06-07 16:45:24 +0200

    Added Italian translation

---
+++ messages/it/cropper.php
@@ -0,0 +1,11 @@
+return [
+    'DEFAULT_LABEL' => 'Clicca e seleziona o trascina un file in quest\'area',
+    'CROP_PHOTO' => 'Taglia immagine',
+    'DELETE_PHOTO' => 'Cancella Immagine',
+    'ERROR_CAN_NOT_UPLOAD_FILE' => 'Non riesco a caricare il file',
+    'MISSING_ATTRIBUTE' => 'L\'attributo "{attribute}" deve essere specificato',
+    'ONLY_POST_REQUEST' => 'Sono permesse solo richieste POST',
+    'UPLOAD_ANOTHER_PHOTO' => 'Carica un\'altra immagine',
+    'TOO_BIG_ERROR' => 'Sono permesse solo immagini con dimensioni non superiori a {size} MB',
+    'EXTENSION_ERROR' => 'Sono permessi sono i seguenti formati immagine: {formats}'
+];
